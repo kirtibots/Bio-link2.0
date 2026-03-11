ad462056e253ad7ee7ea8c7e7d1a5ead2952f966
kirtibots <kritikumari8665@gmail.com>
2026-03-12 00:14:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,43 +1,29 @@
-#  ─────────────────────────────────────
-#        BioLink Bot Configuration
-#  ─────────────────────────────────────
-
 import os
-from config import BOT_USERNAME
+from os import getenv
 
-# ---------------- BOT INFO ----------------
-API_ID = int(os.getenv("API_ID", "21692000"))
-API_HASH = os.getenv("API_HASH", "1e37856155373adf855c061c49847ced")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+# ------------------------------------------------
 
-BOT_USERNAME = os.getenv("BOT_USERNAME", "Biolinkkirtibot")
+API_ID = int(os.environ.get("API_ID", "28795512"))
+API_HASH = os.environ.get("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a")
 
-# ---------------- DATABASE ----------------
-MONGO_URL = os.getenv("MONGO_URL", "")
+# ------------------------------------------------
 
-# ---------------- LOGS ----------------
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1003670001038"))
-OTHER_LOGS = int(os.getenv("OTHER_LOGS", "-1003315572386"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Mafiyamusic_bot")
 
-# ---------------- CHANNELS ----------------
-SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "https://t.me/Kirti_update")
-UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "https://t.me/kirti_supprot_group")
+# -----------------------------------------------
 
-# ---------------- OWNER ----------------
-OWNER_ID = int(os.getenv("OWNER_ID", "5857831018"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "8364016757"))
+SPECIAL_ID = int(os.environ.get("SPECIAL_ID", "8364016757"))
+# ------------------------------------------------
 
-# ---------------- BOT SETTINGS ----------------
-START_IMG = os.getenv(
-    "START_IMG",
-    "https://files.catbox.moe/28m00r.jpg"
-)
+# ------------------------------------------------
+LOGGER_ID = int(os.environ.get("LOGGER_ID", ""))
 
-# ---------------- WARN SETTINGS ----------------
-MAX_WARNINGS = int(os.getenv("MAX_WARNINGS", "3"))
+OTHER_LOGS = int(os.environ.get("OTHER_LOGS", ""))
 
-# ---------------- HEROKU ----------------
-HEROKU_API_KEY = os.getenv("HEROKU_API_KEY", None)
-HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME", None)
+# ------------------------------------------------
 
-# ---------------- BOT VERSION ----------------
-BOT_VERSION = "2.0"
+MONGO_URL = os.environ.get("MONGO_URL", "")
+
+# ------------------------------------------------
